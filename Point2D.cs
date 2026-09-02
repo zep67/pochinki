a849ceeb84c8251b26d47062f744ea6e70c07c62
SAPR <SAPR@PC-NXT>
2026-09-02 12:22:22 +0700

    добавллен класс point2d

---
+++ Point2D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pochinki
+{
+    public class Point2D
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void AddX(int x)
+        {
+            X += x;
+        }
+        public void AddY(int y)
+        {
+            Y = y;
+        }
+
+    }
+    
+}
